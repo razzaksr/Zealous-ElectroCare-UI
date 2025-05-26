cd13b5d0ca72af939e9fe982f938779a8b9a475c
razzaksr <razzaksr@gmail.com>
2025-05-26 16:07:37 +0530

    index js modified

--- src/App.js
+++ src/App.js
@@ -1,38 +1,7 @@
-import { BrowserRouter, Route, Routes} from "react-router-dom";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
-import { Header } from "./Components/Header";
-import Footer from "./Components/Footer";
-import { Service } from "./pages/Service";
-import { NewBooking } from "./pages/NewBooking";
-import { Home } from "./pages/Home";
-import { ViewBookings } from "./pages/ViewBookings";
-import { Signup } from "./pages/Signup";
-import { ErrorPage } from "./pages/ErrorPage";
-import { Login } from "./pages/Login";
-
-
 const App = () => {
   return(
     <>
-      <BrowserRouter>
-        <Header/>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/services" element={<Service/>} />
-          <Route path="/signup" element={<Signup/>} />
-          <Route path="/login" element={<Login/>} />
-          {/* <Route path="/" element={<Landing/>} /> */}
-          <Route path="/error/:error" element={<ErrorPage/>} />
-          {(localStorage.getItem("logged"))?<>
-            <Route path="/book/:service" element={<NewBooking/>} />
-            <Route path="/bookings" element={<ViewBookings/>} />
-          </>:
-          <></>
-          }
-        </Routes>
-        <Footer/>
-      </BrowserRouter>
+      
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+import { Header } from "./Components/Header";
+import Footer from "./Components/Footer";
+import { Service } from "./pages/Service";
+import { NewBooking } from "./pages/NewBooking";
+import { Home } from "./pages/Home";
+import { ViewBookings } from "./pages/ViewBookings";
+import { Signup } from "./pages/Signup";
+import { ErrorPage } from "./pages/ErrorPage";
+import { Login } from "./pages/Login";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/services" element={<Service/>} />
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/login" element={<Login/>} />
+          {/* <Route path="/" element={<Landing/>} /> */}
+          <Route path="/error/:error" element={<ErrorPage/>} />
+          {(localStorage.getItem("logged"))?<>
+            <Route path="/book/:service" element={<NewBooking/>} />
+            <Route path="/bookings" element={<ViewBookings/>} />
+          </>:
+          <></>
+          }
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
   </React.StrictMode>
 );
